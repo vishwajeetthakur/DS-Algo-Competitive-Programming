a87558d14bc74ff276fab3e31697ec1209873cbd
Indal Kumar <73450527+indalkumaryadav@users.noreply.github.com>
2020-11-11 19:53:29 +0530

    Create SingleNumber.cpp

---
+++ Indal Kumar/SingleNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int r=0;
+        for(int i=0;i<nums.size();i++){
+            r=r^nums[i];
+            
+        }
+        
+        return r;
+    }
+};
